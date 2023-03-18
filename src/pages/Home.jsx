2de547d1562f309abe1d3a034f10154669c9f7f8
SametErdogan67 <samedi6774@gmail.com>
2023-03-18 21:18:31 +0300

    react inclass

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Appointment from "../components/Appointment";
+import Doctors from "../components/Doctors";
+import { doctorData } from "../helper/data";
+
+const Home = () => {
+  const [doctors, setDoctors] = useState(doctorData);
+
+  console.log(doctors);
+  return (
+    <main className="text-center mt-2 vh-100">
+      <h1>Mert & Melis Hospital</h1>
+      <Doctors />
+      <Appointment />
+    </main>
+  );
+};
+
+export default Home;
